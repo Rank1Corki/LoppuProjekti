378e233305f6439d6db64fb4345bb09bd920ed53
Rank1Corki <s2200901@edu.bc.fi>
2024-10-25 14:50:51 +0300

    Cannon still WIP
    Cannon almost ready

---
+++ Assets/Scripts/CannonBallSelector.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class CannonballSelector : MonoBehaviour
+{
+    public GameObject cannonballPrefab; // Assign the cannonball prefab in the inspector
+
+    private void OnMouseDown()
+    {
+        // Check if the object was clicked
+        ObjectSelector1 objectSelector = FindObjectOfType<ObjectSelector1>(); // Find the ObjectSelector1 instance
+        if (objectSelector != null)
+        {
+            objectSelector.SelectCannonball(cannonballPrefab); // Select this cannonball prefab
+            Debug.Log("Cannonball Selected: " + cannonballPrefab.name);
+        }
+    }
+}
